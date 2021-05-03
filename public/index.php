06baddf14441dd9e6cda21da06d9bcc6d4372a8e
audreyantony <web2020.audrey@gmail.com>
2021-05-03 13:08:03 +0200

    start index and addition of swiftmailer

--- public/index.php
+++ public/index.php
@@ -1,1 +1,27 @@
 <?php
+
+// TWIG
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+
+// OVH
+use \Ovh\Api;
+use GuzzleHttp\Client;
+
+// SESSION
+session_start();
+
+// CONST ROOT
+define('ROOT', dirname(__DIR__));
+
+// MAIN DEPENDENCIES
+require_once ROOT . "/bin/config.php";
+require_once ROOT . "/vendor/autoload.php";
+
+// AUTOLOAD
+spl_autoload_register(
+    function ($className) {
+        require ROOT . "/model/" . $className . ".php";
+    }
+);
