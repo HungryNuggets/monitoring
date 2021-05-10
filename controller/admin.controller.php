0efabafb3ca7c3bf801bb807169c72c6e0841e6f
audreyantony <web2020.audrey@gmail.com>
2021-05-10 09:44:02 +0200

    Meta and admin controller

--- controller/admin.controller.php
+++ controller/admin.controller.php
@@ -1,3 +1,33 @@
 <?php
 
-echo "Admin";
+// ADMIN HOME => CUSTOMER LIST
+if (isset($_GET['customer'])){
+
+    require ROOT."/controller/admin/customer.admin.controller.php";
+
+    exit();
+}
+
+// ISSUE PAGE => LIST AND CONTACT INFO
+if (isset($_GET['issue'])) {
+
+    require ROOT."/controller/admin/issue.admin.controller.php";
+
+    exit();
+}
+
+// ADMIN PAGE (LIST)
+if (isset($_GET['admin'])) {
+
+    require ROOT."/controller/admin/admin.admin.controller.php";
+
+    exit();
+}
+
+// DISCONNECTION
+if (isset($_GET['disconnection'])) {
+
+    AdminManager::disconnection();
+    header("Location: ./");
+    exit();
+}
