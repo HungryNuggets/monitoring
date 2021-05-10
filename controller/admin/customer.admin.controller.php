ff920648254cd96bca5b83341dfc2a93c057aad7
audreyantony <web2020.audrey@gmail.com>
2021-05-10 13:25:24 +0200

    preparation controller customer

--- controller/admin/customer.admin.controller.php
+++ controller/admin/customer.admin.controller.php
@@ -1,5 +1,29 @@
 <?php
 
+// CREATE CUSTOMER
+if (isset($_GET['create'])){
+
+    require ROOT . "controller/admin/crud/crud.create.customer.controller.php";
+
+    exit();
+}
+
+// UPDATE CUSTOMER
+if (isset($_GET['update']) && ctype_digit($_GET['update'])){
+
+    require ROOT . "controller/admin/crud/crud.update.customer.controller.php";
+
+    exit();
+}
+
+// UPDATE CUSTOMER
+if (isset($_GET['delete']) && ctype_digit($_GET['delete'])){
+
+    require ROOT . "controller/admin/crud/crud.delete.customer.controller.php";
+
+    exit();
+}
+
 // CUSTOMER GLOBAL DISPLAY WITH OPTIONS
 $customerManager = new CustomerManager($DB);
 
