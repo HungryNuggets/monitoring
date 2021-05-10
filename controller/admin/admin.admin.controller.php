73bb2eb597907c64257b7b440fb3c1b3176089bb
audreyantony <web2020.audrey@gmail.com>
2021-05-10 12:27:33 +0200

    admin controller

--- controller/admin/admin.admin.controller.php
+++ controller/admin/admin.admin.controller.php
@@ -1,4 +1,12 @@
 <?php
 
+// ISSUES GLOBAL DISPLAY WITH OPTIONS
+$adminManager = new AdminManager($DB);
+
 // TWIG RENDER
-echo $twig->render("public/home.html.twig",["admin"=>'yes']);
+echo $twig->render("admin/admin.html.twig",
+    [
+        "admin"=>'yes',
+        "session"=>$_SESSION,
+        "allAdmin"=>$adminManager->selectAllAdmin()
+    ]);
